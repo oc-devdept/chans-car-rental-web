98261186c46c26c72f9b6104746997245c0ceb8b
ccometz <sohweikiat@hotmail.com>
2019-12-18 11:46:11 +0800

    clean up

--- redux/reducers.js
+++ redux/reducers.js
@@ -5,14 +5,9 @@ import { combineReducers } from "redux";
 
 import { LeadReducer } from "Ducks/lead";
 
-// Redux Form
-import { createStore } from 'redux'
-
 const reducers = combineReducers({
   // reducers
   leadState: LeadReducer
 });
-
-const store = createStore(reducers)
 
 export default reducers;
